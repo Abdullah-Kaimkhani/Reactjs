ae008974df87c79d81b3fd7253895aa6b435af0b
Abdullah Kaimkhani <abdullahjavedkk@gmail.com>
2024-12-15 17:59:35 +0500

    Add files via upload

---
+++ Class01/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDom from "react-dom/client"
+const rootDiv = document.getElementById("root");
+ReactDom.createRoot(rootDiv).render(
+  <div>
+    <h1>Assalaam-u-Alaikum, Abdullah Kaimkhani here</h1>
+    <img src="https://th.bing.com/th/id/OIP.R7IUTUPDNFKHdZ-AyuACxgHaE8?rs=1&pid=ImgDetMain" alt="Fastest Car" />
+    <p>It's the fastest car in the world.</p>
+  </div>
+)
