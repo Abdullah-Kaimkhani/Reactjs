1eaa939469d2b134720f684b62e5f0bbcf1a8fd3
Abdullah Kaimkhani <abdullahjavedkk@gmail.com>
2025-01-11 21:27:35 +0500

    registration form with its column

---
+++ Class/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Registration_form from './Screens/Registration_form'
+import Table from './Components/Table'
+
+const App = () => {
+  return (
+    <>
+    <Registration_form />
+    <Table />
+    </>
+  )
+}
+
+export default App
