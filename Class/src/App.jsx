f7fc6c4ac415055a93a385774787e901d39aec71
Abdullah Kaimkhani <abdullahjavedkk@gmail.com>
2025-01-18 21:24:28 +0500

    Add files via upload

---
+++ Class/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Registration_form from './Screens/Registration_form'
+
+const App = () => {
+  return (
+    <>
+    <Registration_form />
+    </>
+  )
+}
+
+export default App
