47301da203f98d6c30b59d2c5007e3e1a9bdae3f
Abdullah Kaimkhani <abdullahjavedkk@gmail.com>
2025-01-26 18:01:15 +0500

    Add files via upload

---
+++ New folder (5)/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Product from './Components/Product';
+import CardDetails from './Components/CardDetails';
+import PrimarySearchAppBar from './Components/Header';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Product/>} />
+        <Route path='/CardDeatail/:id' element={<CardDetails/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
